468e433e49d72e26153f7a9172582e24f956a7b7
Dylan Gavin <dyl.gavin@gmail.com>
2022-11-15 11:13:33 -0800

    start spire users model

--- music_app/app/models/user.rb
+++ music_app/app/models/user.rb
@@ -1,2 +1,37 @@
 class User < ApplicationRecord
+    validates :email, presence: true
+
+    attr_reader: password
+
+
+    def self.find_by_credentials(username, password)
+        @user = User.find_by(username: username)
+        if @user && @user.is_password?(password)
+            @user
+        else
+            nil
+        end
+    end
+
+    def password=(password)
+        @password = password
+        password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+
+    end
+
+    def reset_session_token
+
+    end
+
+    private
+    def ensure_session_token
+
+    end
+
+    def generate_session_token
+
+    end
 end
